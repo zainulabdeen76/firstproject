c0079e386cc9401ee78fa10c3861a8fac16fbe77
zainulabdeen76 <zainchchch@gmail.com>
2021-03-19 13:31:48 +0500

    new project

---
+++ lib/iPhone12ProMax1.dart
@@ -0,0 +1,376 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+class iPhone12ProMax1 extends StatelessWidget {
+  iPhone12ProMax1({
+    Key key,
+  }) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color(0xffffffff),
+      body: Stack(
+        children: <Widget>[
+          Transform.translate(
+            offset: Offset(-452.0, -167.0),
+            child:
+                // Adobe XD layer: 'wallpapersden.com_a…' (shape)
+                Container(
+              width: 1268.0,
+              height: 713.0,
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                  image: const AssetImage('assets/images/b.jpg'),
+                  fit: BoxFit.fill,
+                ),
+              ),
+            ),
+          ),
+          Transform.translate(
+            offset: Offset(159.0, 513.0),
+            child: Container(
+              width: 57.0,
+              height: 57.0,
+              decoration: BoxDecoration(
+                borderRadius:
+                    BorderRadius.all(Radius.elliptical(9999.0, 9999.0)),
+                color: const Color(0xffe41a58),
+              ),
+            ),
+          ),
+          Transform.translate(
+            offset: Offset(20.0, 23.0),
+            child: SvgPicture.string(
+              _svg_4weait,
+              allowDrawingOutsideViewBox: true,
+            ),
+          ),
+          Transform.translate(
+            offset: Offset(150.0, 55.0),
+            child: Text(
+              'February',
+              style: TextStyle(
+                fontFamily: 'Segoe UI',
+                fontSize: 20,
+                color: const Color(0xffffffff),
+                fontWeight: FontWeight.w300,
+              ),
+              textAlign: TextAlign.left,
+            ),
+          ),
+          Transform.translate(
+            offset: Offset(176.0, 84.0),
+            child: Text(
+              '2015',
+              style: TextStyle(
+                fontFamily: 'Segoe UI',
+                fontSize: 12,
+                color: const Color(0xffc8c8c8),
+                fontWeight: FontWeight.w300,
+              ),
+              textAlign: TextAlign.left,
+            ),
+          ),
+          Transform.translate(
+            offset: Offset(138.0, 135.0),
+            child: Container(
+              width: 99.0,
+              height: 99.0,
+              decoration: BoxDecoration(
+                borderRadius:
+                    BorderRadius.all(Radius.elliptical(9999.0, 9999.0)),
+                border: Border.all(width: 3.0, color: const Color(0xffff0000)),
+              ),
+            ),
+          ),
+          Transform.translate(
+            offset: Offset(162.0, 161.0),
+            child: Text(
+              '54',
+              style: TextStyle(
+                fontFamily: 'Segoe UI',
+                fontSize: 35,
+                color: const Color(0xffffffff),
+                fontWeight: FontWeight.w300,
+              ),
+              textAlign: TextAlign.left,
+            ),
+          ),
+          Transform.translate(
+            offset: Offset(201.0, 168.0),
+            child: Text(
+              '%',
+              style: TextStyle(
+                fontFamily: 'Segoe UI',
+                fontSize: 15,
+                color: const Color(0xffffffff),
+                fontWeight: FontWeight.w300,
+              ),
+              textAlign: TextAlign.left,
+            ),
+          ),
+          Transform.translate(
+            offset: Offset(174.0, 537.0),
+            child: Container(
+              width: 6.0,
+              height: 20.0,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(5.0),
+                color: const Color(0xffffffff),
+              ),
+            ),
+          ),
+          Transform.translate(
+            offset: Offset(185.0, 526.0),
+            child: Container(
+              width: 6.0,
+              height: 31.0,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(5.0),
+                color: const Color(0xffffffff),
+              ),
+            ),
+          ),
+          Transform.translate(
+            offset: Offset(196.0, 531.0),
+            child: Container(
+              width: 6.0,
+              height: 26.0,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(5.0),
+                color: const Color(0xffffffff),
+              ),
+            ),
+          ),
+          Transform.translate(
+            offset: Offset(287.0, 168.0),
+            child: Container(
+              width: 71.0,
+              height: 71.0,
+              decoration: BoxDecoration(
+                borderRadius:
+                    BorderRadius.all(Radius.elliptical(9999.0, 9999.0)),
+                border: Border.all(width: 3.0, color: const Color(0xffc500ff)),
+              ),
+            ),
+          ),
+          Transform.translate(
+            offset: Offset(23.0, 168.0),
+            child: Container(
+              width: 71.0,
+              height: 71.0,
+              decoration: BoxDecoration(
+                borderRadius:
+                    BorderRadius.all(Radius.elliptical(9999.0, 9999.0)),
+                border: Border.all(width: 3.0, color: const Color(0xff0800ff)),
+              ),
+            ),
+          ),
+          Transform.translate(
+            offset: Offset(50.0, 194.0),
+            child: Text(
+              '24',
+              style: TextStyle(
+                fontFamily: 'Segoe UI',
+                fontSize: 16,
+                color: const Color(0xffffffff),
+                fontWeight: FontWeight.w300,
+              ),
+              textAlign: TextAlign.left,
+            ),
+          ),
+          Transform.translate(
+            offset: Offset(316.0, 193.0),
+            child: Text(
+              '18',
+              style: TextStyle(
+                fontFamily: 'Segoe UI',
+                fontSize: 16,
+                color: const Color(0xffffffff),
+                fontWeight: FontWeight.w300,
+              ),
+              textAlign: TextAlign.left,
+            ),
+          ),
+          Transform.translate(
+            offset: Offset(330.0, 194.0),
+            child: Text(
+              '%',
+              style: TextStyle(
+                fontFamily: 'Segoe UI',
+                fontSize: 7,
+                color: const Color(0xffffffff),
+                fontWeight: FontWeight.w300,
+              ),
+              textAlign: TextAlign.left,
+            ),
+          ),
+          Transform.translate(
+            offset: Offset(67.0, 194.0),
+            child: Text(
+              '%',
+              style: TextStyle(
+                fontFamily: 'Segoe UI',
+                fontSize: 7,
+                color: const Color(0xffffffff),
+                fontWeight: FontWeight.w300,
+              ),
+              textAlign: TextAlign.left,
+            ),
+          ),
+          Transform.translate(
+            offset: Offset(49.0, 618.0),
+            child: Text(
+              'Completed',
+              style: TextStyle(
+                fontFamily: 'Segoe UI',
+                fontSize: 16,
+                color: const Color(0xff000000),
+                fontWeight: FontWeight.w300,
+              ),
+              textAlign: TextAlign.left,
+            ),
+          ),
+          Transform.translate(
+            offset: Offset(49.0, 661.0),
+            child: Text(
+              'Snoozed',
+              style: TextStyle(
+                fontFamily: 'Segoe UI',
+                fontSize: 16,
+                color: const Color(0xff000000),
+                fontWeight: FontWeight.w300,
+              ),
+              textAlign: TextAlign.left,
+            ),
+          ),
+          Transform.translate(
+            offset: Offset(49.0, 704.0),
+            child: Text(
+              'Overdue',
+              style: TextStyle(
+                fontFamily: 'Segoe UI',
+                fontSize: 16,
+                color: const Color(0xff000000),
+                fontWeight: FontWeight.w300,
+              ),
+              textAlign: TextAlign.left,
+            ),
+          ),
+          Transform.translate(
+            offset: Offset(26.0, 625.0),
+            child: Container(
+              width: 9.0,
+              height: 9.0,
+              decoration: BoxDecoration(
+                borderRadius:
+                    BorderRadius.all(Radius.elliptical(9999.0, 9999.0)),
+                color: const Color(0xffff00cc),
+              ),
+            ),
+          ),
+          Transform.translate(
+            offset: Offset(26.0, 668.0),
+            child: Container(
+              width: 9.0,
+              height: 9.0,
+              decoration: BoxDecoration(
+                borderRadius:
+                    BorderRadius.all(Radius.elliptical(9999.0, 9999.0)),
+                color: const Color(0xff00e5ff),
+              ),
+            ),
+          ),
+          Transform.translate(
+            offset: Offset(26.0, 712.0),
+            child: Container(
+              width: 9.0,
+              height: 9.0,
+              decoration: BoxDecoration(
+                borderRadius:
+                    BorderRadius.all(Radius.elliptical(9999.0, 9999.0)),
+                color: const Color(0xff00ff22),
+              ),
+            ),
+          ),
+          Transform.translate(
+            offset: Offset(314.0, 618.0),
+            child: Text(
+              '108',
+              style: TextStyle(
+                fontFamily: 'Segoe UI',
+                fontSize: 16,
+                color: const Color(0xff000000),
+                fontWeight: FontWeight.w300,
+              ),
+              textAlign: TextAlign.left,
+            ),
+          ),
+          Transform.translate(
+            offset: Offset(317.0, 661.0),
+            child: Text(
+              '56',
+              style: TextStyle(
+                fontFamily: 'Segoe UI',
+                fontSize: 16,
+                color: const Color(0xff000000),
+                fontWeight: FontWeight.w300,
+              ),
+              textAlign: TextAlign.left,
+            ),
+          ),
+          Transform.translate(
+            offset: Offset(317.0, 704.0),
+            child: Text(
+              '36',
+              style: TextStyle(
+                fontFamily: 'Segoe UI',
+                fontSize: 16,
+                color: const Color(0xff000000),
+                fontWeight: FontWeight.w300,
+              ),
+              textAlign: TextAlign.left,
+            ),
+          ),
+          Transform.translate(
+            offset: Offset(56.0, 373.0),
+            child: SizedBox(
+              width: 264.0,
+              child: Text(
+                'Good Job you\'ve completed 6%\nmore tasks this month',
+                style: TextStyle(
+                  fontFamily: 'Segoe UI',
+                  fontSize: 18,
+                  color: const Color(0xffffffff),
+                  fontWeight: FontWeight.w300,
+                ),
+                textAlign: TextAlign.center,
+              ),
+            ),
+          ),
+          Transform.translate(
+            offset: Offset(341.0, 15.0),
+            child:
+                // Adobe XD layer: 'Wallmax-3318' (shape)
+                Container(
+              width: 40.0,
+              height: 40.0,
+              decoration: BoxDecoration(
+                borderRadius:
+                    BorderRadius.all(Radius.elliptical(9999.0, 9999.0)),
+                image: DecorationImage(
+                  image: const AssetImage('assets/images/a.jpg'),
+                  fit: BoxFit.cover,
+                ),
+                border: Border.all(width: 1.0, color: const Color(0xff707070)),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+const String _svg_4weait =
+    '<svg viewBox="20.0 23.0 321.5 55.7" ><path transform="translate(330.5, 64.66)" d="M 10.99980068206787 7.000200271606445 L 0 14.00040054321289 L 10.99980068206787 7.000200271606445 Z M 0 0 L 10.99980068206787 7.000200271606445 L 0 0 Z" fill="none" stroke="#ffffff" stroke-width="1" stroke-miterlimit="4" stroke-linecap="butt" /><path transform="matrix(-1.0, 0.0, 0.0, -1.0, 48.5, 78.67)" d="M 10.99978637695313 7.000200271606445 L 0 0 L 10.99978637695313 7.000200271606445 Z M 0 14.00040054321289 L 10.99978637695313 7.000200271606445 L 0 14.00040054321289 Z" fill="none" stroke="#ffffff" stroke-width="1" stroke-miterlimit="4" stroke-linecap="butt" /><path transform="translate(20.0, 23.0)" d="M 1.999800086021423 19.99979972839355 C 0.8955000042915344 19.99979972839355 0 19.10519981384277 0 18 C 0 16.89570045471191 0.8955000042915344 16.00020027160645 1.999800086021423 16.00020027160645 L 20.99970054626465 16.00020027160645 C 22.10400009155273 16.00020027160645 22.9995002746582 16.89570045471191 22.9995002746582 18 C 22.9995002746582 19.10519981384277 22.10400009155273 19.99979972839355 20.99970054626465 19.99979972839355 L 1.999800086021423 19.99979972839355 Z M 1.999800086021423 11.99970054626465 C 0.8955000042915344 11.99970054626465 0 11.10510063171387 0 9.999899864196777 C 0 8.895600318908691 0.8955000042915344 8.000100135803223 1.999800086021423 8.000100135803223 L 20.99970054626465 8.000100135803223 C 22.10400009155273 8.000100135803223 22.9995002746582 8.895600318908691 22.9995002746582 9.999899864196777 C 22.9995002746582 11.10510063171387 22.10400009155273 11.99970054626465 20.99970054626465 11.99970054626465 L 1.999800086021423 11.99970054626465 Z M 1.999800086021423 3.999600172042847 C 0.8955000042915344 3.999600172042847 0 3.105000019073486 0 1.999800086021423 C 0 0.8955000042915344 0.8955000042915344 0 1.999800086021423 0 L 20.99970054626465 0 C 22.10400009155273 0 22.9995002746582 0.8955000042915344 22.9995002746582 1.999800086021423 C 22.9995002746582 3.105000019073486 22.10400009155273 3.999600172042847 20.99970054626465 3.999600172042847 L 1.999800086021423 3.999600172042847 Z" fill="#ffffff" stroke="none" stroke-width="1" stroke-miterlimit="4" stroke-linecap="butt" /></svg>';
---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_app2/iPhone12ProMax1.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: iPhone12ProMax1(),
+    );
+  }
+}
